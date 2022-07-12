02e3024acdae1ef8f11065d259cb8edd31cbaea1
joelramos <joelramos456@gmail.com>
2022-07-11 19:54:52 -0500

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import BoxText from "./components/BoxText"
-import BoxImg from './components/BoxImg'
 import foto1 from './img/foto1.jpg'
 import foto2 from './img/foto2.jpg'
 
 function App() {
-  const img3 = 'https://drive.google.com/uc?export=view&id=1CyAlt0zK_mhrDQacVC4M4Zeferwxp5PQ'
-  return(
-    <div>
-      {/* HEADER */}
+  return (
+    <div className="container">
       <header>
-        <h1 className="header-title">Nuestra Historia</h1>
+        <h1>Nuestra Historia</h1>
       </header>
-
-      {/* MAIN */}
-      <div className="main-table">
-        <BoxText title='Costa Verde' date='17/03/2022'/>
-        <BoxImg img={img3}/>
-        <BoxImg img={foto2}/>
-        <BoxText title='Mar de miraflores' date='17/03/2022'/>
+      <div className="a">
+        <h1>Costa verde</h1>
+        <p>17/07/2022</p>
+      </div>
+      <div className="b">
+        <img className="img-box" src={foto1} alt="" />
+      </div>
+      <div className="c">
+        <img className="img-box" src={foto2} alt="" />
+      </div>
+      <div className="d">
+        <h1>Costa verde</h1>
+        <p>17/07/2022</p>
       </div>
     </div>
   )
--- src/components/BoxImg.jsx
+++ src/components/BoxImg.jsx
@@ -1,6 +1,7 @@
 function BoxImg(props) {
   return (
     <div className="boximg">
+      <p>Esto es una imagen</p>
       <img className="img-box" src={props.img} alt="" />
     </div>
   );
